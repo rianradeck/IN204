52d110901886974eb362dfba34538474ac189bb8
Rian Radeck <rianradeck3@gmail.com>
2024-09-18 11:10:08 +0200

    Messing with pre-commit hooks

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # IN204
+
+## Formatters, Linters and Static Code Analyzers
+Installation steps for the hooks used by pre-commit
+```ps1
+# To install choco
+winget install --id=Chocolatey.Chocolatey -e
+
+# To install the hooks
+choco install llvm uncrustify cppcheck
+pip install cpplint
+
+# To run the hooks
+pip install pre-commit
+pre-commit install
+pre-commit run
+```
--- code.cpp
+++ code.cpp
@@ -1,7 +1,39 @@
-#include <bits/stdc++.h>
+struct MyCounter
+{
+    unsigned counter;
+    unsigned max;
 
-using namespace std;
+    unsigned getCounter() const
+    {
+        return counter;
+    }
+    unsigned getMax() const
+    {
+        return max;
+    }
 
-int main(){
-    cout << "Teste\n";
-}
+    void increment()
+    {
+        counter++;
+        if (counter > max)
+            counter = 0;
+    }
+
+    void reset()
+    {
+        counter = 0;
+    }
+
+    void set(unsigned value)
+    {
+        counter = (value <= max) ? value : counter;
+    }
+
+    void setMax(unsigned value)
+    {
+        if (counter >= value)
+            counter = 0;
+        max = value;
+        ;
+    }
+};
