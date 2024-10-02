c1a54955b094a0969c90824cafd7ae76415ef066
Rian Radeck <rianradeck3@gmail.com>
2024-10-02 09:17:06 +0200

    End of td1

--- td1/counter.hpp
+++ td1/counter.hpp
@@ -1,4 +1,18 @@
+#include <iostream>
+
 struct MyCounter {
+    MyCounter() : counter(0), max(0) {
+    }
+    MyCounter(unsigned max) : MyCounter(0, max) {
+    }
+    MyCounter(unsigned counter, unsigned max) : counter(counter), max(max) {
+    }
+    MyCounter(MyCounter const& other) : counter(other.counter), max(other.max) {
+    }
+    ~MyCounter() {
+        std::cout << "Deleting (" << counter << ", " << max << ")\n";
+    }
+
     unsigned counter;
     unsigned max;
 
